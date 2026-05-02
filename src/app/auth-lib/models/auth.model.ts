ffac8929e4a53eeb3f2ac099a67170ecca3a2702
Ludo35300 <ludo35300@gmail.com>
2026-05-02 17:01:11 +0200

    maj auth.model

--- src/app/auth-lib/models/auth.model.ts
+++ src/app/auth-lib/models/auth.model.ts
@@ -6,11 +6,24 @@ export type MeResponse = {
     lastName: string;
 };
 
+export type LoginRequest = {
+  email: string;
+  password: string;
+};
+
 export type RegisterRequest = {
     email: string;
     firstName: string;
     lastName: string;
     password: string;
+};
+export type ForgotPasswordRequest = {
+  email: string;
+};
+
+export type ResetPasswordRequest = {
+  token: string;
+  password: string;
 };
 
 /** Réponse simple pour endpoints qui répondent { ok: true } */
